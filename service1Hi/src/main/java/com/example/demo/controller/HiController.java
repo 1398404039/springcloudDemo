82a0e361ca98f4bf01f741a121b2fb0ff5263919
liweizhong <1398404039@qq.com>
2018-04-02 14:11:13 +0800

    修改配置文件
    修改配置文件

---
+++ service1Hi/src/main/java/com/example/demo/controller/HiController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Value("${server.port}")
+    String port;
+    @Value("${author}")
+    String author;
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(){
+        return "This is Hi Service,port: " + port + "  and author: " + author;
+    }
+}
